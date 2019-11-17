f0e33a062d764a618d8b62b2d39b2d5841a0dc14
Enrique Ubillus <ubillus.e@gmail.com>
2019-11-17 14:54:34 +0100

    Added:  - IngredientExtention.css  - IngredientBTO.cs
    Edited:
     - Participant - AfficherMeny.css (DisplayIngredients() created)
     - ParticipantTests.cs (GetTestsListOfIngredient / BTO_DisplayIngredients_In_English_Test / BTO_DisplayIngredients_In_French_Test  /         public void BTO_DisplayIngredients_In_Dutch_Test()
    ) created

---
+++ SandwichSystem.Shared/BTO/IngredientBTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SandwichSystem.Shared.BTO
+{
+    public class IngredientBTO
+    {
+        public string Name { get; set; }
+    }
+}
